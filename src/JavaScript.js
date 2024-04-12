0e6e3fd5923341cd6332e6ac5bd8320c27298536
alja1313 <olgaslahota@gmail.com>
2024-04-12 19:46:16 +0200

    Add Basic Search Engine in JS

---
+++ src/JavaScript.js
@@ -0,0 +1,9 @@
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
