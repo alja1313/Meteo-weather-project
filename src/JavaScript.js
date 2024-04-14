fe49a7677c74394535d9977a1712f270441a4527
alja1313 <olgaslahota@gmail.com>
2024-04-14 15:18:11 +0200

    Add Search functionality using API

--- src/JavaScript.js
+++ src/JavaScript.js
@@ -4,6 +4,23 @@ searchFormElement.addEventListener("submit", handleSearchSubmit);
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
+
+function searchCity(city) {
+  let apiKey = "3t1efbo10d0b2a5b53045a2c4a8666fe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+searchCity("Singapore");
